ec947ccf085d0786864e07a2dc9e38c0118c8c80
ajclarkin <mail@ajclarkin.co.uk>
2023-01-02 13:44:23 +0000

    Working on day 13 part 1

---
+++ day13-distress_signal/README.md
@@ -0,0 +1,3 @@
+# Day 13 - Distress Signal
+
+Ok, first problem - how do I parse the input? Usual techniques result in a string and I want to use list methods.
---
+++ day13-distress_signal/part1.py
@@ -0,0 +1,57 @@
+# Day 13 - Distress Signal
+
+pairs = list(map(str.splitlines, open('example.txt').read().strip().split('\n\n')))
+
+def CompareLeftRight(l, r):
+    for k, left in enumerate(l):
+
+        if len(r) == k:
+            # Got to end of item and left is longer than right
+            # eg if k = 3 then we're looking for item 4 (zero-index) and so if len = 3 we have a problem
+            print('Oh dear, end of the road')
+            return False
+        else:
+            right = r[k]
+
+        print(f'Compare {left} with {right}\t\t{type(left)} - {type(right)}')
+
+        if type(left) == int and type(right) == list:
+            temp = left
+            left = list()
+            left.append(temp)
+
+        if type(right) == int and type(left) == list:
+            temp = right
+            right = list()
+            right.append(temp)
+
+        if type(left) == int and type(right) == int:
+            if left < right:
+                print('Return True - int comparison')
+                return True
+            elif right > left:
+                print('Return False - int comparison')
+                return False
+
+
+        if type(left) == list and type(right) == list:
+            for ll, rr in zip(left, right):
+                if ll < rr:
+                    print('Return True - int in list comparison')
+                    return True
+                elif rr > ll:
+                    print('Return False - int in list comparison')
+                    return False
+
+            if len(left) < len(right):
+                print('Return True - list length comparison')
+                return True
+            elif len(right) < len(left):
+                print('Return False - list length comparison')
+                return False
+        
+
+
+
+for l, r in pairs:
+    CompareLeftRight(eval(l), eval(r))
