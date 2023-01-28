b1f2e323cf61b18a72b57e941c9bfcf51a248f16
ajclarkin <mail@ajclarkin.co.uk>
2023-01-28 13:23:20 +0000

    Solution to day 18 part 2

--- day18-boiling_boulders/part2b.py
+++ day18-boiling_boulders/part2b.py
@@ -6,11 +6,20 @@ elements = [x for x in open('input.txt').read().strip().split('\n')]
 boulders = []
 max_x, max_y, max_z = 0, 0, 0
 
+for e in elements:
+    b = [int(v) for v in e.split(',')]
+    boulders.append(b)
+
+    x, y, z = b
+    max_x = x if x > max_x else max_x
+    max_y = y if y > max_y else max_y
+    max_z = z if z > max_z else max_z
+
+
+
 queue = deque()
 tested = []
 pockets = []
-not_pockets = []
-part_1 = 3526
 
 
 adjacent = [
@@ -22,29 +31,27 @@ adjacent = [
     [0, 0, 1]
 ]
 
-for e in elements:
-    b = [int(v) for v in e.split(',')]
-    boulders.append(b)
 
-    x, y, z = b
-    max_x = x if x > max_x else max_x
-    max_y = y if y > max_y else max_y
-    max_z = z if z > max_z else max_z
+# Identify every possible block space
+possibles = [[x, y, z] for x in range(max_x+1) for y in range(max_y+1) for z in range(max_z+1)]
 
-print(max_x, max_y, max_z)
-possibles = [[x, y, z] for x in range(1, max_x+1) for y in range(1, max_y+1) for z in range(1, max_z+1)]
 
-max_possibles = len(possibles)
+# From the possible blocks we want to remove those which contain boulders.
+# We can't edit the list while iterating through it so we use a copy.
+
 possibles_working = possibles.copy()
 for p in possibles:
     if p in boulders:
         possibles_working.remove(p)
 possibles = possibles_working.copy()
 
-p_count = 0
+
+
+# Find all the pockets - those non-boulder areas completely surrounded by boulders.
+# This is a repeated breadth-first search.
+
 for p in possibles:
     if p not in tested:
-        # print(f'Testing {p}\t\t{len(pockets)} {len(not_pockets)}')
         current = []
         queue.clear()
 
@@ -58,21 +65,18 @@ for p in possibles:
 
             # Check if it's at the edge
             x, y, z = q
-            if x == 1 or y == 1 or z == 1 or x == max_x or y == max_y or z == max_z:
+            if x == 0 or y == 0 or z == 0 or x == max_x or y == max_y or z == max_z:
                 edge = True
 
 
             adj_b = [list(map(sum, zip(q, a))) for a in adjacent]
             for a in adj_b:
-                if (0 < a[0] < max_x+1) and (0 < a[1] < max_y+1) and (0 < a[2] < max_z+1) \
-                    and a not in current and a not in boulders and a not in queue and a not in tested:
+                if 0 <= a[0] < max_x+1 and 0 <= a[1] < max_y+1 and 0 <= a[2] < max_z+1 \
+                    and a not in current and a not in boulders and a not in queue:
                     queue.append(a)
+
         if edge == False:
             pockets.extend(current)
-            p_count += 1
-        else:
-            not_pockets.extend(current)
-
 
 
 
@@ -87,26 +91,5 @@ for b in boulders:
 
     faces += f
 
-
-
-
                 
-print('boulders', len(boulders))
-print('pockets', len(pockets))
-print('not_pockets', len(not_pockets))
-print('max_possibles', max_possibles)
-print('p_count', p_count)
-print('faces', faces)
-print('tested', len(tested))
-
-print()
-
-s1 = set()
-for p in not_pockets:
-    x,y,z = p
-    s1.add((x, y, z))
-
-print(set, len(s1))
-
-
-# 2096 too high
+print('Faces', faces)
